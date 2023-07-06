0404ff9ec115fa61bf975f1cc120eda48c7eea66
Yanislav Igonin <yanislavdabest@gmail.com>
2023-07-06 22:39:07 +0300

    add get response method

--- src/ai.ts
+++ src/ai.ts
@@ -1,7 +1,31 @@
+import { replies } from './replies';
 import { config } from '@/config';
+import { type ChatCompletionRequestMessage } from 'openai';
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
   apiKey: config.openAiApiKey,
 });
 export const openai = new OpenAIApi(configuration);
+
+export const addUserContext = (text: string): ChatCompletionRequestMessage => {
+  return {
+    content: text,
+    role: 'user',
+  };
+};
+
+export const getAiResponse = async (
+  prompt: string,
+  context: ChatCompletionRequestMessage[] = [],
+  model = 'gpt-4-32k',
+) => {
+  const userMessage = addUserContext(prompt);
+  const messages = [...context, userMessage];
+  const response = await openai.createChatCompletion({
+    messages,
+    model,
+  });
+  const text = response.data.choices[0].message?.content;
+  return text?.trim() ?? replies.error;
+};
