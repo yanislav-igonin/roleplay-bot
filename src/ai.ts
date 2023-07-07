c9c9813a4f9ec69a3abcdb62d79d0dfe91b1fd21
Yanislav Igonin <yanislavdabest@gmail.com>
2023-07-07 10:32:11 +0300

    gms prompts wip

--- src/ai.ts
+++ src/ai.ts
@@ -8,12 +8,40 @@ const configuration = new Configuration({
 });
 export const openai = new OpenAIApi(configuration);
 
+export const addSystemContext = (
+  text: string,
+): ChatCompletionRequestMessage => {
+  return {
+    content: text,
+    role: 'system',
+  };
+};
+
+export const addAssistantContext = (
+  text: string,
+): ChatCompletionRequestMessage => {
+  return {
+    content: text,
+    role: 'assistant',
+  };
+};
+
 export const addUserContext = (text: string): ChatCompletionRequestMessage => {
   return {
     content: text,
     role: 'user',
   };
 };
+
+const gameRules =
+  `Each time players try to do something game master` +
+  `(GM) will roll a d20 dice for this action. ` +
+  `Every result equals orhigher than 10 is a success. ` +
+  `If the result is lower than 10 it's a failure. `;
+const gameMasterPromt =
+  `You're a game master. ` +
+  `You're in charge of a game that is similar to Dungeon and Dragons ` +
+  `roleplay game but with a simplier rules.`;
 
 export const getAiResponse = async (
   prompt: string,
