3afee90b3b7de46b87f90bcf6a27b6d0e1a48e92
Yanislav Igonin <yanislavdabest@gmail.com>
2023-07-07 10:34:51 +0300

    export new models

--- src/database.ts
+++ src/database.ts
@@ -3,4 +3,9 @@ import { PrismaClient } from '@prisma/client';
 export { type User } from '@prisma/client';
 
 export const database = new PrismaClient();
-export const { user: userModel } = database;
+export const {
+  user: userModel,
+  context: contextModel,
+  game: gameModel,
+  inventory: inventoryModel,
+} = database;
