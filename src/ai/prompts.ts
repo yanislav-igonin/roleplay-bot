c7bf5bacf0e9134dda5b494789be1bff49bd6337
Yanislav Igonin <yanislavdabest@gmail.com>
2023-07-22 18:23:20 +0300

    changing prompt a little

--- src/ai/prompts.ts
+++ src/ai/prompts.ts
@@ -1,21 +1,29 @@
 import { Language } from 'locale';
 
+export const gmPrompt =
+  `You're a game master.\n` +
+  `You're in charge of a game that is similar to Dungeon and Dragons.\n`;
+
+export const rulesPrompt =
+  'Rules is simple: each time players try to do something game master ' +
+  'they should roll a d20 dice for this action.\n' +
+  'Every result equals or higher than 10 is a success.\n' +
+  '1 and 20 are critical fails and critical successes respectively. ' +
+  'They should produce a more interesting result.';
+
 export const markdownRules =
   `Text should be formatted in Markdown. ` +
   `You can use ONLY the following formatting without any exceptions:` +
   `**bold text**, *italic text*, ~~strikethrough~~.`;
 
 export const getNewGamePrompt = (language = Language.Ru) =>
-  `You're a game master.\n` +
-  `You're in charge of a game that is similar to Dungeon and Dragons ` +
-  `roleplay game but with a simplier rules.\n` +
-  `Your task is to make a quest for a new game in ${language} language.\n` +
+  `Your task is to make a quest for a new roleplay game in ${language} language.\n` +
   `You can use any setting you want.\n` +
   `Describe a world briefly, its inhabitants and where the heroes located.\n` +
   `Describe a quest that players will have to complete.\n` +
   `All description should not be longer than 1000 characters.\n` +
   `${markdownRules}\n` +
-  `For the line breaks "\n" symbols should be used.\n` +
+  `For the line breaks "\n" symbols should be used, DO NOT USE ACTUAL LINE BREAK.\n` +
   `Format output as a JSON - ` +
   `{"name": "Game name", "description": "Game description"}.`;
 
@@ -23,8 +31,6 @@ export const getNewCharacterPrompt = (
   gameDescription: string,
   language = Language.Ru
 ) =>
-  `You're a game master.\n` +
-  `You're in charge of a game that is similar to Dungeon and Dragons.\n` +
   `Make a new character for the game (game description provided below between` +
   `""") in ${language} language.\n` +
   `Character description should not be longer than 300 characters.\n` +
@@ -36,7 +42,7 @@ export const getNewCharacterPrompt = (
   `Be creative, do not create only basic fantasy races like dwarfes, ghnomes, elves, humans. ` +
   `You can generate also some other races like goblins, centaurus, etc.\n` +
   `Character can have some items, skills, spells, etc.\n` +
-  `For the line breaks "\n" symbols should be used.\n` +
+  `For the line breaks "\n" symbols should be used, DO NOT USE ACTUAL LINE BREAK.\n` +
   `Format output as a JSON - ` +
   `{"name": "Character name", "description": "Character description"}.\n` +
   `Game description:\n` +
@@ -56,24 +62,16 @@ export const getSummaryForImageGenerationPrompt = (text: string) =>
   `\nText:` +
   `"""${text}"""`;
 
-export const gmPrompt =
-  `You're a game master.\n` +
-  `You're in charge of a game that is similar to Dungeon and Dragons.\n`;
-export const rulesPrompt =
-  'Rules is simple: each time players try to do something game master ' +
-  'they should roll a d20 dice for this action.\n' +
-  'Every result equals or higher than 10 is a success.\n' +
-  '1 and 20 are critical fails and critical successes respectively. ' +
-  'They should produce a more interesting result.';
-
 export const getFirstContextPrompt = (
   gameDescription: string,
   characterDescription: string
 ) =>
   gmPrompt +
+  markdownRules +
   `Based on the game description and a character description provided below between """ ` +
   `make a description of where our characters located right now before the quest.\n` +
   `It can be a tavern, a forest, a cave, etc.\n` +
+  `Description should not be longer than 500 characters.\n` +
   `For the line breaks "\n" symbols should be used.\n` +
   `Game description:\n` +
   `"""${gameDescription}"""\n\n` +
